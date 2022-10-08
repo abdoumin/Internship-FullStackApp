e4d2cd44ebe5b73816bb268d3e4fdb9edb0224ce
abdoumin <78703693+abdoumin@users.noreply.github.com>
2022-10-08 20:43:11 +0100

    Added adress to CORS

--- Internship-Backend-main/src/main/java/com/example/demo/controllers/Controller.java
+++ Internship-Backend-main/src/main/java/com/example/demo/controllers/Controller.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @CrossOrigin(origins={"http://localhost:3000","http://localhost:3001","http://baridestivage.xyz/",
-        "http://165.22.67.244:6868"})
+        "http://165.22.67.244:6868/"})
 public class Controller {
 
     @Autowired
