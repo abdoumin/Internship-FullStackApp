e601b70d5ea3794a6f2bbf885d596df291f69f0b
abdoumin <78703693+abdoumin@users.noreply.github.com>
2022-10-08 20:31:13 +0100

    Added adress to CORS

--- Internship-Backend-main/src/main/java/com/example/demo/security/controller/EmployeeController.java
+++ Internship-Backend-main/src/main/java/com/example/demo/security/controller/EmployeeController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
-@CrossOrigin(origins={"http://localhost:3000","http://baridestivage.xyz/"})
+@CrossOrigin(origins={"http://localhost:3000","http://baridestivage.xyz/","http://165.22.67.244:6868"})
 public class EmployeeController {
     @RequestMapping(value = "/greeting", method = RequestMethod.GET)
     public String getEmployees() {
